8c99cf53438d5eee81f3582f3c3edf96a3505ab8
dmesa2 <dmesa2@pdx.edu>
2020-07-08 19:48:26 -0700

    implemented some of the code

--- RomanNumerals/src/test/java/edu/pdx/cs410J/dmesa2/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/dmesa2/KataTest.java
@@ -1,13 +1,26 @@
 package edu.pdx.cs410J.dmesa2;
 
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class KataTest
 {
 
   @Test
   public void canInstantiateKataClass() {
+
     new Kata();
+  }
+
+  @Test
+  public void romanNumeralFor1isI(){
+    assertThat(Kata.romanNumeralFor(1), equalTo("I"));
+  }
+
+  @Test
+  public void romanNumeralFor5isV(){
+       assertThat(Kata.romanNumeralFor(5), equalTo("V"));
   }
 
 }
