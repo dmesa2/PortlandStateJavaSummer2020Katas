bc6f69e92511ed794009e1154a6a4723f8b06628
swet09 <67349200+swet09@users.noreply.github.com>
2020-07-23 09:30:46 -0700

    Update KataTest.java
    Completed Add, Sub, Multiply, Div, MIN, MAX for 2 inputs

--- rpn/src/test/java/edu/pdx/cs410J/dmesa2/KataTest.java
+++ rpn/src/test/java/edu/pdx/cs410J/dmesa2/KataTest.java
@@ -17,8 +17,42 @@ public class KataTest
   }
 
   @Test
-  public void canInstantiateKataClass() {
-    new Kata();
+  public void plusAdd(){
+    String input = "20 5 +";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(25));
   }
 
+  @Test
+  public void plusSub(){
+    String input = "20 5 -";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(15));
+  }
+
+  @Test
+  public void plusMul(){
+    String input = "20 5 *";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(100));
+  }
+
+  @Test
+  public void Max(){
+    String input = "5 9 MAX";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(9));
+  }
+  @Test
+  public void Min(){
+    String input = "5 9 MIN";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(5));
+  }
+  
 }
