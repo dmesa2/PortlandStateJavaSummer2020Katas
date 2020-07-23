867b92804dc9b7ff786b53717f5e0ad690d1ace3
swet09 <67349200+swet09@users.noreply.github.com>
2020-07-23 09:29:24 -0700

    Update Kata.java
    Completed Add, Sub, Multiply, Div, MIN, MAX for 2 inputs

--- rpn/src/main/java/edu/pdx/cs410J/dmesa2/Kata.java
+++ rpn/src/main/java/edu/pdx/cs410J/dmesa2/Kata.java
@@ -10,18 +10,75 @@ import java.util.Stack;
  */
 public class Kata {
                                                                                     
-
-  public static void main(String[] args) {
+public static void main(String[] args) {
     System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  public static boolean isNumeric(String strNum) {
+    if (strNum == null) {
+      return false;
+    }
+    try {
+      double d = Double.parseDouble(strNum);
+    } catch (NumberFormatException nfe) {
+      return false;
+    }
+    return true;
   }
 
   public int calculator(String _input) {
     Stack<String> stack = new Stack<String>();
     String[] input = _input.split(" ");
-    for (String i: input){
-      stack.push(i);
+    int value=0;
+    for (String i :input){
+      if(isNumeric(i))
+      {
+        System.out.println("number "+ i);
+        stack.push(i);
+      }
+      else
+      {
+        int val1 = Integer.parseInt(stack.pop().toString());
+        int val2 = Integer.parseInt(stack.pop().toString());
+        switch (i)
+        {
+          case "+":
+            value = val2 + val1;
+            stack.push(Integer.toString(value));
+            break;
+
+          case "-":
+            value = val2 - val1;
+            stack.push(Integer.toString(value));
+            break;
+
+          case "/":
+            value = val2 / val1;
+            stack.push(Integer.toString(value));
+            break;
+
+          case "*":
+            value = val2 * val1;
+            stack.push(Integer.toString(value));
+            break;
+
+          case "MAX":
+            value = Math.max(val2,val1);
+            stack.push(Integer.toString(value));
+            stack.push("MAX");
+            break;
+
+          case "MIN":
+            value = Math.min(val2,val1);
+            stack.push(Integer.toString(value));
+            stack.push("MIN");
+            break;
+
+        }
+      }
+
     }
-    return 0;
+    return Integer.parseInt(stack.pop());
   }
 }
