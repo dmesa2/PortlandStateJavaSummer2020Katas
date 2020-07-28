02d8a2c63e2e9762d2007d0598856fee920b274d
swet09 <67349200+swet09@users.noreply.github.com>
2020-07-28 00:01:19 -0700

    Update KataTest.java
    included SQRT and test cases

--- rpn/src/test/java/edu/pdx/cs410J/dmesa2/KataTest.java
+++ rpn/src/test/java/edu/pdx/cs410J/dmesa2/KataTest.java
@@ -41,18 +41,48 @@ public class KataTest
   }
 
   @Test
-  public void Max(){
-    String input = "5 9 MAX";
+  public void Min(){
+    String input = "5 9 1 3 MIN";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(1));
+  }
+
+  @Test
+  public void test1(){
+    String input = "4 2 + 3 -";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(3));
+  }
+
+  @Test
+  public void test2(){
+    String input = "3 5 8 * 7 + *";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(141));
+  }
+
+  @Test
+  public void Test3(){
+    String input = "5 3 4 2 9 1 MAX";
     Kata kata = new Kata();
     int result = kata.calculator(input);
     assertThat(result, equalTo(9));
   }
   @Test
-  public void Min(){
-    String input = "5 9 MIN";
+  public void Test4(){
+    String input = "4 5 MAX 1 2 MAX *";
     Kata kata = new Kata();
     int result = kata.calculator(input);
-    assertThat(result, equalTo(5));
+    assertThat(result, equalTo(10));
   }
-  
+  @Test
+  public void Test5(){
+    String input = "9 SQRT";
+    Kata kata = new Kata();
+    int result = kata.calculator(input);
+    assertThat(result, equalTo(3));
+  }
 }
